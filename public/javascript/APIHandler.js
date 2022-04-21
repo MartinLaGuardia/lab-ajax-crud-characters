4bd2461c0eb18f147fc314f0b977ebae03244d08
Jose Pereira suarez <johndoe@example.com>
2022-04-21 09:19:59 +0200

    1

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,33 @@
+
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+
+  constructor(baseUrl) {
+    this.axiosApp = axios.create({
+      baseURL: baseUrl,
+
+    })
   }
 
-  getFullList () {
+  getFullList() {
+    return this.axiosApp.get('/characters')
+  }
+
+  getOneRegister = (registerId) => {
+    return this.axiosApp.get(`/characters/${registerId}`)
+  }
+
+  createOneRegister() {
+    return this.axiosApp.post('/characters')
+  }
+
+  updateOneRegister = (updateId) => {
+    return this.axiosApp.put(`/characters/${updateId}`)
 
   }
 
-  getOneRegister () {
+  deleteOneRegister = (deleteId) => {
+    return this.axiosApp.post(`/characters/${deleteId}`)
 
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
 
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,64 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('https://minions-api.herokuapp.com/');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
 
-  });
+    charactersAPI
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+      .getFullList()
+      .then(response => {
 
-  });
+        let text
+        response.data.forEach(eachCharacter => {
+          text +=
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+            `<div class= "character-info">
+        <div class="name">${eachCharacter.name}</div>
+        <div class="occupation">${eachCharacter.cartoon}</div>
+        <div class="cartoon">${eachCharacter.id}</div>
+        <div class="weapon">${eachCharacter.weapon}</div>
+      </div>`
 
-  });
+          document.querySelector('.characters-container').innerHTML = text
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+        })
 
-  });
+      }).catch(err => console.log(err));
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  })
+})
 
-  });
-});
+document.getElementById('fetch-one').addEventListener('click', function (event) {
+
+  charactersAPI
+
+    .getOneRegister(registerId)
+    .then(elm => {
+      let text
+      response.data(registerId => {
+        text +=
+
+
+          `< div class="operation" >
+            <label for="character-id">${registerId}</label>
+            <input type="text" name="character-id">
+              <button id="fetch-one">Fetch one</button>
+          </div>`
+
+        document.querySelector('.operation').innerHTML = result
+
+
+      });
+
+      document.getElementById('delete-one').addEventListener('click', function (event) {
+
+
+      });
+
+      document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+
+      });
+
+      document.getElementById('new-character-form').addEventListener('submit', function (event) {
+
+      });
